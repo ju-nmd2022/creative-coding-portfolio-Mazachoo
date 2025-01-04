33fdce9ba921bab25880e1a4fef387653f02ec47
Mazachoo <reneta.iv.nikolova@gmail.com>
2025-01-04 16:21:51 +0200

    Molnar variations mistake

--- experiments/Molnar-Dialog-variation.js
+++ experiments/Molnar-Dialog-variation.js
@@ -1,19 +1,3 @@
-function setup() {
-  createCanvas(1000, 1000);
-  frameRate(2);
-}
-
-const size = 160;
-const layers = 13;
-
-//color (255, 255, 255), //white
-//color (200, 5, 20), //red
-//color (55, 188, 25), //green
-//color (15, 35, 250), //blue
-//color (125, 235, 250), //light blue
-//color (240, 245, 15), //yellow
-//color (160, 60, 235), //purple
-
 const colors = [
   color(255, 214, 255),
   color(231, 198, 255),
@@ -22,19 +6,26 @@ const colors = [
   color(187, 208, 255),
   color(247, 37, 133),
 ];
+
+function setup() {
+  createCanvas(1000, 1000);
+  frameRate(2);
+}
+
+const size = 160;
+const layers = 13;
+
 function getRandomValue(pos, variance) {
   return pos + map(Math.random(), 0, 0.12, -variance, variance);
 }
 
 function drawLayers(x, y, size, layers) {
-  // const half = size / 2;
   const variance = size / 50;
   noFill();
   strokeWeight(1);
 
   stroke(random(colors));
 
-  //rectMode(CENTER);
   for (let i = 0; i < layers; i++) {
     if (Math.random() > 0.98) {
       continue;
@@ -59,7 +50,6 @@ function drawLayers(x, y, size, layers) {
       getRandomValue(y + half, variance)
     );
     endShape(CLOSE);
-    //rect(x - half, y - half, s, s);
   }
 }
 
@@ -70,5 +60,4 @@ function draw() {
       drawLayers(size / 2 + x * size, size / 2 + y * size, size, layers);
     }
   }
-  //noLoop();
 }
